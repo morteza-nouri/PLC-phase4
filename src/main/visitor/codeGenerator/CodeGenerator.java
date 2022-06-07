ea146f3cc15f99793b23dcac3c5680c7aab0e137
Daniel Saeedi <saeedi.danial@gmail.com>
2022-06-07 11:59:06 +0430

    Getting Started

--- src/main/visitor/codeGenerator/CodeGenerator.java
+++ src/main/visitor/codeGenerator/CodeGenerator.java
@@ -42,10 +42,14 @@ public class CodeGenerator extends Visitor<String> {
     private ClassDeclaration currentClass;
     private MethodDeclaration currentMethod;
 
+    private int labelCount;
+
     public CodeGenerator(Graph<String> classHierarchy) {
         this.classHierarchy = classHierarchy;
         this.expressionTypeChecker = new ExpressionTypeChecker(classHierarchy);
         this.prepareOutputFolder();
+
+        this.labelCount = 0;
     }
 
     private void prepareOutputFolder() {
@@ -141,25 +145,143 @@ public class CodeGenerator extends Visitor<String> {
     public String visit(ClassDeclaration classDeclaration) {
         String name = classDeclaration.getClassName().getName();
         createFile(name);
-        //todo
+        //todo: done
+
+        this.currentClass = classDeclaration;
+
+        addCommand(".class "+name);
+
+        if (classDeclaration.getParentClassName() == null)
+        {
+            addCommand(".super java/lang/Object");
+        }
+        else
+        {
+            String class_name_parent = classDeclaration.getParentClassName().getName();
+            addCommand(".super " + class_name_parent);
+        }
+
+        for(FieldDeclaration field : classDeclaration.getFields())
+        {
+            field.accept(this);
+        }
+
+        if(classDeclaration.getConstructor() != null)
+        {
+            this.currentMethod = classDeclaration.getConstructor();
+            this.expressionTypeChecker.setCurrentMethod(classDeclaration.getConstructor());
+            classDeclaration.getConstructor().accept(this);
+        }
+        else
+            defaultConstructorDecl();
+
+        for(MethodDeclaration m : classDeclaration.getMethods())
+        {
+            this.currentMethod = m;
+            this.expressionTypeChecker.setCurrentMethod(m);
+            m.accept(this);
+        }
+
+        return null;
+    }
+
+    // Not Done
+    private void defaultConstructorDecl() {
+        String className = currentClass.getClassName().getName();
+
+        addCommand(".method public <init>()V");
+        addCommand(".limit stack 128");
+        addCommand(".limit locals 128");
+        addCommand("aload 0");
+
+        if (currentClass.getParentClassName() == null){
+            addCommand("invokespecial java/lang/Object/<init>()V");
+        }
+        else{
+            String parentClassName = currentClass.getParentClassName().getName();
+            addCommand("invokespecial " + parentClassName + "/<init>()V");
+        }
+
+
+//        for(FieldDeclaration field : currentClass.getFields()){
+//            String fieldName = field.getVarDeclaration().getVarName().getName();
+//            Type fieldType = field.getVarDeclaration().getType();
+//
+//            if(fieldType instanceof ClassType || fieldType instanceof FptrType){
+//                addCommand("aload 0");
+//                addCommand("aconst_null");
+//                addCommand("putfield " + className + "/" + fieldName + " L" + makeTypeFlag(fieldType) + ";\n");
+//            }
+//            else if(fieldType instanceof IntType || fieldType instanceof BoolType){
+//                addCommand("aload 0");
+//                addCommand("ldc 0");
+//                if(fieldType instanceof IntType)
+//                    addCommand("invokestatic java/lang/Integer/valueOf(I)Ljava/lang/Integer;");
+//                if(fieldType instanceof BoolType)
+//                    addCommand("invokestatic java/lang/Boolean/valueOf(Z)Ljava/lang/Boolean;");
+//                addCommand("putfield " + className + "/" + fieldName + " L" + makeTypeFlag(fieldType) + ";\n");
+//            }
+//            else if(fieldType instanceof StringType){
+//                addCommand("aload 0");
+//                addCommand("ldc \"\"");
+//                addCommand("putfield " + className + "/" + fieldName + " L" + makeTypeFlag(fieldType) + ";\n");
+//            }
+//            else{
+//                addCommand("aload 0");
+//                initializeList((ListType) fieldType);
+//                addCommand("putfield " + className + "/" + fieldName + " L" + makeTypeFlag(fieldType) + ";\n");
+//            }
+//        }
+        addCommand("return");
+        addCommand(".end method");
+    }
+
+    // Not done yet
+    private String makeTypeFlag(Type t)
+    {
+        if (t instanceof IntType)
+            return "java/lang/Integer";
+        if (t instanceof BoolType)
+            return "java/lang/Boolean";
+        if (t instanceof ArrayType)
+            return "Array";
+        if (t instanceof FptrType)
+            return "Fptr";
+        if (t instanceof ClassType)
+            return ((ClassType)t).getClassName().getName();
         return null;
     }
 
     @Override
     public String visit(ConstructorDeclaration constructorDeclaration) {
-        //todo
+        //todo: done
+        if (constructorDeclaration.getArgs().size() > 0)
+            defaultConstructorDecl();
+
+        String class_name = this.currentClass.getClassName().getName();
+
+        if (class_name.equals("Main"))
+            addStaticMainMethod();
+
+        this.visit((MethodDeclaration) constructorDeclaration);
+
         return null;
     }
 
     @Override
     public String visit(MethodDeclaration methodDeclaration) {
         //todo
+
         return null;
     }
 
     @Override
     public String visit(FieldDeclaration fieldDeclaration) {
-        //todo
+        //todo: done
+        String field_name = fieldDeclaration.getVarDeclaration().getVarName().getName();
+        Type feild_type = fieldDeclaration.getVarDeclaration().getType();
+        String flag = makeTypeFlag(feild_type);
+        addCommand(".field " + field_name + " L" + flag + ";");
         return null;
     }
 
@@ -171,19 +293,41 @@ public class CodeGenerator extends Visitor<String> {
 
     @Override
     public String visit(AssignmentStmt assignmentStmt) {
-        //todo
+        //todo: done
+        BinaryExpression assign_expression = new BinaryExpression(assignmentStmt.getlValue(), assignmentStmt.getrValue(), BinaryOperator.assign);
+        addCommand(assign_expression.accept(this));
+        addCommand("pop");
         return null;
     }
 
     @Override
     public String visit(BlockStmt blockStmt) {
-        //todo
+        //todo: done
+        for (Statement stmt: blockStmt.getStatements())
+            stmt.accept(this);
         return null;
+    }
+
+    private String getNewLabel(){
+        String l = "Label_";
+        l += this.labelCount;
+        this.labelCount += 1;
+        return l;
     }
 
     @Override
     public String visit(ConditionalStmt conditionalStmt) {
-        //todo
+        //todo: done
+        String false_label = getNewLabel();
+        String after = getNewLabel();
+        addCommand(conditionalStmt.getCondition().accept(this));
+        addCommand("ifeq " + false_label);
+        conditionalStmt.getThenBody().accept(this);
+        addCommand("goto " + after);
+        addCommand(false_label + ":");
+        if (conditionalStmt.getElseBody() != null)
+            conditionalStmt.getElseBody().accept(this);
+        addCommand(after + ":");
         return null;
     }
 
@@ -195,19 +339,42 @@ public class CodeGenerator extends Visitor<String> {
 
     @Override
     public String visit(MethodCallStmt methodCallStmt) {
-        //todo
+        //todo: done
+        this.expressionTypeChecker.setIsInMethodCallStmt(true);
+        addCommand(methodCallStmt.getMethodCall().accept(this));
+        addCommand("pop");
+        this.expressionTypeChecker.setIsInMethodCallStmt(false);
         return null;
     }
 
     @Override
     public String visit(PrintStmt print) {
-        //todo
+        //todo: done
+        addCommand("getstatic java/lang/System/out Ljava/io/PrintStream;");
+        Type argument_type = print.getArg().accept(expressionTypeChecker);
+        addCommand(print.getArg().accept(this));
+        if (argument_type instanceof IntType)
+            addCommand("invokevirtual java/io/PrintStream/print(I)V");
+        if (argument_type instanceof BoolType)
+            addCommand("invokevirtual java/io/PrintStream/print(Z)V");
         return null;
     }
 
     @Override
     public String visit(ReturnStmt returnStmt) {
-        //todo
+        //todo: done
+        Type return_type = returnStmt.getReturnedExpr().accept(expressionTypeChecker);
+        if(return_type instanceof NullType) {
+            addCommand("return");
+            return null;
+        }
+
+        addCommand( returnStmt.getReturnedExpr().accept(this) );
+        if(return_type instanceof IntType)
+            addCommand("invokestatic java/lang/Integer/valueOf(I)Ljava/lang/Integer;");
+        if(return_type instanceof BoolType)
+            addCommand("invokestatic java/lang/Boolean/valueOf(Z)Ljava/lang/Boolean;");
+        addCommand("areturn");
         return null;
     }
 
